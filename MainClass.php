ab8d6d03a8615608bedc6567f15fd9f7f895de70
minejonasdev <57543213+minejonasdev@users.noreply.github.com>
2019-11-09 07:37:42 +0100

    Update MainClass.php

--- MainClass.php
+++ MainClass.php
@@ -1,1 +1,20 @@
+<?php
 
+namespace: CommandPlugin;
+
+use pocketmine/PluginBase
+use pocketmine/command/Command
+use pocketmine/command/CommandSender
+ 
+class Main extends PluginBase
+{
+  public function onEnable()
+  {
+    $this->getLogger()->info("Plugin aktiviert")
+  }
+  
+  public function onCommand(CommandSender,$sender,Command,$cmd,$label, array $args)
+    
+
+
+  
